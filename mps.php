e2e9788833f3f3960b9a9dfb09b9cd02abc3ae34
appleraju <46217412+appleraju@users.noreply.github.com>
2020-12-19 17:49:51 +0530

    Update mps.php

--- mps.php
+++ mps.php
@@ -318,7 +318,7 @@ padding-top: 5px;
         $(this).toggleClass('menu-opened');
         var mainmenu = $(this).next('ul');
         if (mainmenu.hasClass('open')) {
-          mainmenu.slideToggle().removeClass('open');
+          mainmenu.slideToggle().removeClass('open');sadasfsdf
         } else {
           mainmenu.slideToggle().addClass('open');
           if (settings.format === "dropdown") {
